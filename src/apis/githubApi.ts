5f215cab23a5ecc28bb752c3eb6d15a06b8436ef
dboccala <david@cordage.io>
2024-06-03 15:01:37 -0300

    add dotenv and update configs of github token

--- src/apis/githubApi.ts
+++ src/apis/githubApi.ts
@@ -1,6 +1,23 @@
 import axios from 'axios';
 
+const VITE_GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+
+if (!VITE_GITHUB_TOKEN) {
+	throw new Error('GitHub token is not defined in the environment variables');
+}
+
 export const githubApi = axios.create({
 	baseURL: 'https://api.github.com/repos/facebook/react',
-	headers: {},
+	headers: {
+		Authorization: `Bearer ${VITE_GITHUB_TOKEN}`,
+	},
 });
+
+githubApi
+	.get('')
+	.then((response) => {
+		console.log(response.data);
+	})
+	.catch((error) => {
+		console.error('Error making request:', error);
+	});

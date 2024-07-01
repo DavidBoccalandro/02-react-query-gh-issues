e5a132104a73f0b0f2777580d5ddb0b2e045d3aa
DavidBoccalandro <davidboccalandro@hotmail.com>
2024-07-01 15:55:03 -0300

    add date conversion helper and read of labels and dates

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -12,10 +12,13 @@ interface Props {
 const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]> => {
 	await sleep(2);
 
-	const params = new URLSearchParams
+	const params = new URLSearchParams();
 
 	if (state) params.append('state', state);
 	if (labels.length > 0) params.append('labels', labels.join(','));
+
+	params.append('page', '1');
+	params.append('per_page', '5');
 
 	const { data } = await githubApi.get<Issue[]>('/issues', { params });
 	return data;

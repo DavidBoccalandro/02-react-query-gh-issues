f6ccd1633dc0b2eed7ebee39236f741582dd8749
DavidBoccalandro <davidboccalandro@hotmail.com>
2024-07-01 15:09:45 -0300

    add complex queryKey use

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -2,15 +2,26 @@ import { useQuery } from 'react-query';
 import { githubApi } from '../../apis/githubApi';
 import { sleep } from '../../helpers/sleep';
 import { Issue } from '../interfaces';
+import { State } from '../interfaces/issue';
 
-const getIssues = async (): Promise<Issue[]> => {
+interface Props {
+	state?: State;
+	labels: string[];
+}
+
+const getIssues = async (labels: string[] = [], state?: State): Promise<Issue[]> => {
 	await sleep(2);
 
-	const { data } = await githubApi.get<Issue[]>('/issues');
+	const params = new URLSearchParams
+
+	if (state) params.append('state', state);
+	if (labels.length > 0) params.append('labels', labels.join(','));
+
+	const { data } = await githubApi.get<Issue[]>('/issues', { params });
 	return data;
 };
 
-export const useIssues = () => {
-	const issuesQuery = useQuery(['issues'], getIssues);
+export const useIssues = ({ state, labels }: Props) => {
+	const issuesQuery = useQuery(['issues', { state, labels }], () => getIssues(labels, state));
 	return { issuesQuery };
 };

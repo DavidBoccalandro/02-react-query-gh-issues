cea787b97a3a64e26676bd649f5d070c6fce23d7
DavidBoccalandro <davidboccalandro@hotmail.com>
2024-07-01 11:29:48 -0300

    add prefetchQuery

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { FiCheckCircle, FiInfo, FiMessageSquare } from 'react-icons/fi';
+import { useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router';
+import { getIssueComments, getIssueInfo } from '../hooks/useIssue';
 import { Issue, State } from '../interfaces/issue';
 
 interface Props {
@@ -9,9 +11,29 @@ interface Props {
 
 export const IssueItem: FC<Props> = ({ issue }: Props) => {
 	const navigate = useNavigate();
+	const queryClient = useQueryClient(); // Esto es para poder hacer prefetch de los datos de los issues.
+
+	const onMouseEnter = () => {
+		// prefetchQuery toma dos parámetros:
+		// 1. La clave de la consulta (query key), que es un array que identifica de manera única la consulta.
+		//    En este caso, ['issue', issue.number] y ['issue', issue.number, 'comments'].
+		// 2. Una función que retorna una promesa con los datos que queremos prefetch.
+		//    Aquí usamos getIssueInfo y getIssueComments para obtener la información del issue y sus comentarios.
+
+		// Otra sintaxis es pasarlo en un objeto de opciones, ejemplo:
+		// queryClient.prefetchQuery({
+		// queryKey: ['issue', issue.number],
+		// queryFn: () => getIssueInfo(issue.number.toString()) });
+		queryClient.prefetchQuery(['issue', issue.number], () => getIssueInfo(issue.number.toString()));
+		queryClient.prefetchQuery(['issue', issue.number, 'comments'], () => getIssueComments(issue.number.toString()));
+	};
 
 	return (
-		<div className="card mb-2 issue" onClick={() => navigate(`/issues/issue/${issue.number}`)}>
+		<div
+			className="card mb-2 issue"
+			onClick={() => navigate(`/issues/issue/${issue.number}`)}
+			onMouseEnter={onMouseEnter}
+		>
 			<div className="card-body d-flex align-items-center">
 				{issue.state === State.Open ? <FiInfo size={30} color="red" /> : <FiCheckCircle size={30} color="green" />}
 
@@ -31,3 +53,5 @@ export const IssueItem: FC<Props> = ({ issue }: Props) => {
 		</div>
 	);
 };
+
+// Si hago en el mouse enter, las peticiones, claramente va a hacer muchísimas peticiones. Entonces hay que considerar cómo manejarlo.
--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -3,14 +3,14 @@ import { githubApi } from '../../apis/githubApi';
 import { sleep } from '../../helpers/sleep';
 import { Issue } from '../interfaces';
 
-const getIssueInfo = async (issueNumber: string): Promise<Issue> => {
+export const getIssueInfo = async (issueNumber: string): Promise<Issue> => {
 	sleep(2);
 	const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`);
 
 	return data;
 };
 
-const getIssueComments = async (issueNumber: string): Promise<Issue[]> => {
+export const getIssueComments = async (issueNumber: string): Promise<Issue[]> => {
 	sleep(2);
 	const { data } = await githubApi.get<Issue[]>(`/issues/${issueNumber}/comments`);
 
